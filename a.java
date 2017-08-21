76032c227fa69f37eb6fae87c4c527825b40ac4c
Jeminas <31195435+Jeminas@users.noreply.github.com>
2017-08-21 15:23:46 +0800

    asdasd
    asdasdasd

--- a.java
+++ a.java
@@ -1,5 +1,15 @@
 package neww;
-
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 public class a {
-试验
+	 public static void main(String agrs[]) {
+		  JFrame frame = new JFrame("管理系统");  
+		  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+		  JLabel label = new JLabel("能力规范");
+		  label.setPreferredSize(new Dimension(1000,700));
+		  frame.getContentPane().add(label, BorderLayout.CENTER);  
+		  frame.pack();
+		  frame.setVisible(true);
+		 }
 }
